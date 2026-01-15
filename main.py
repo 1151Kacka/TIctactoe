d17e840d9ec52e9a29e572cff42337d09ddc0b36
1151Kacka <1151@student.itgymnazium.cz>
2026-01-15 08:59:11 +0100

    uvitaci z

---
+++ main.py
@@ -0,0 +1,19 @@
+"""projekt_2.py: druhý projekt ?
+autor: Kačka Srpová
+mail: 1151@student.itgymnazium.cz"""
+
+def main():
+    # Uvítací zpráva
+     print("Welcome to Tic Tac Toe")
+     print("=" * 80)
+     print("GAME RULES")
+     print("Each player can place one mark (or stone)")
+     print("per turn on the 3x3 grid. The WINNER is")
+     print("who succeeds in placing three of their")
+     print("marks in a:")
+     print("*horizontal")
+     print("*vertical or")
+     print("*diagonal row")
+     print("=" * 80)
+     print("Let's start the game")
+     print("-" * 80)
